f0cd388b02f9f9f7e2d2fefb4eca36b23cf46a60
Your Name <108307350+pjduri@users.noreply.github.com>
2023-10-30 08:51:14 -0500

    data and vars studio variables declared

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,23 @@
 // Initialize Variables below
+let date = 'Monday 2019-03-18'
+let time = '10:05:34'
+let astronautCount = 7
+let astronautStatus = 'ready'
+let averageAstronautMassKg = '80.7'
+let crewMassKg = astronautCount * averageAstronautMassKg
+let fuellMassKg = 760000
+let shuttleMassKg = 74842.31
+let totalMassKg = crewMassKg + fuellMassKg + shuttleMassKg
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = '100%'
+let weatherStatus = 'clear'
+let preparedForLiftoff = true
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+
 
 // add logic below to verify all astronauts are ready
 
