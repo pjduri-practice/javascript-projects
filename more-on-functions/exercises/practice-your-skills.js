a365946a4ea17e0644d36ac39c9230402c344cd3
Your Name <108307350+pjduri@users.noreply.github.com>
2023-11-06 11:16:29 -0600

    func practice

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,6 +5,14 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+const tripleOrPirate = function (num) {
+    if (typeof num === 'number') {
+        return num * 3
+    } else if (typeof num === 'string') {
+        return 'ARR!'
+    }
+    return num
+}
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
@@ -12,4 +20,8 @@ b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
 
-let arr = ['Elocution', 21, 'Clean teeth', 100];
+const arr = ['Elocution', 21, 'Clean teeth', 100];
+
+for (let i=0; i<arr.length; i++) {
+    console.log(tripleOrPirate(arr[i])) 
+}
