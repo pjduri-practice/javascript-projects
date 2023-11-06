fc09f69bdf3aef2fe8f53bf759bdc1e2900f0b1a
Your Name <108307350+pjduri@users.noreply.github.com>
2023-11-06 11:36:40 -0600

    studio find min

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -1,4 +1,13 @@
 //1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
+function findMinimum(numsArr) {
+    let minVal = numsArr[0]
+    for (let i=0; i<numsArr.length; i++) {
+        if (minVal > numsArr[i]) {
+            minVal = numsArr[i]
+        }
+    }
+    return minVal
+}
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
@@ -7,4 +16,6 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(findMinimum(nums1));
+console.log(findMinimum(nums2));
+console.log(findMinimum(nums3));
