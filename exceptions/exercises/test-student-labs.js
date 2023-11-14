b862f1ebb93fa5fa4fd59893647a3e4658ee72ff
Your Name <108307350+pjduri@users.noreply.github.com>
2023-11-14 10:41:34 -0600

    exceptions exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,9 +1,16 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    let output = `${lab.student} code worked: `
+    try {
+      let result = lab.runLab(3);
+      output += result === 27;
+    } catch(e) {
+      output += 'Error thrown.'
+    }
+    console.log(output)
   }
+  console.log()
 }
 
 let studentLabs = [
@@ -22,3 +29,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
