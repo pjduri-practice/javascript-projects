ad8d2360245f44776fbb2eefa8a7b98d9ef365a3
Your Name <108307350+pjduri@users.noreply.github.com>
2023-11-30 10:21:03 -0600

    dom and events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,9 +1,32 @@
 function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+    const missionAbort = document.getElementById("abortMission")
+    const button = document.getElementById("liftoffButton")
+    const paragraph = document.getElementById("statusReport")
+    const resetButton = document.getElementById('reset')
+    const origText = paragraph.innerHTML
 
     // Put your code for the exercises here.
+    button.addEventListener('click', () => {
+        paragraph.innerHTML = 'Houston, we have liftoff!'
+    })
+
+    missionAbort.addEventListener('mouseover', e => {
+        e.target.style.backgroundColor = 'red'
+    })
+    
+    missionAbort.addEventListener('mouseout', e => {
+        e.target.style.backgroundColor = ''
+    })
+
+    missionAbort.addEventListener('click', () => {
+        paragraph.innerHTML = confirm('Are you sure you want to abort the mission?') ? 
+            'Mission aborted!  Space shuttle returning home.' : paragraph.innerHTML
+    })
+
+    // not part of exercise, just more convenient than hitting refresh
+    resetButton.addEventListener('click', () => {
+        paragraph.innerHTML = origText
+    })
     
 }
 
